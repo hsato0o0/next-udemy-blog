48ff2e6aa43152e071464fc57734c76258c7fc87
hsato0o0 <harenotisatou@gmail.com>
2025-05-12 22:51:23 +0900

    section5-57

---
+++ src/components/layouts/PublicHeader.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '../ui/navigation-menu';
+
+export default function PublicHeader() {
+  return (
+    <div>
+      <header className="border-b bg-blue-200">
+        <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+          <NavigationMenu>
+            <NavigationMenuLink>
+              <NavigationMenuItem>
+                <Link href="" legacyBehavior passHref>
+                  <NavigationMenuLink className="font-bold text-xl">
+                    Blog
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuLink>
+          </NavigationMenu>
+          <div className="flex items-center gap-4">
+            <Input
+              placeholder="記事を検索..."
+              className="w-[200px] lg:w-[300px]"
+            />
+            <Button variant="outline" asChild>
+              <Link href="/login">ログイン</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/register">登録</Link>
+            </Button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
---
+++ src/lib/post.ts
@@ -0,0 +1,17 @@
+import { prisma } from './prisma';
+
+export default async function getPosts() {
+  return await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+}
